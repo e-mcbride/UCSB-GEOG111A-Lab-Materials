9993f0ccccb34c99d25576c86a66cb9e2d29152d
Elizabeth McBride <43974009+e-mcbride@users.noreply.github.com>
2018-10-10 10:57:09 -0700

    Lab 1 example of final submission, first commit

---
+++ McBride_Lab1.R
@@ -0,0 +1,12 @@
+# Geog 111A Lab 1
+# Elissa McBride
+
+# Question 1
+1 + 1 # = 2
+
+2 + 2 # = 4
+
+
+
+# Question 4
+## Answering in a comment below the question number so I can tell where it came from. 
